2a5e28358280d8dfe477aba86aada8c6274dde2f
Forrest Jones <fjones-dev@8bitforest.com>
2018-02-18 21:20:22 -0700

    :bug: Fix api crash due to missing files

--- client-electron/js/api/awesome-sms-api.js
+++ client-electron/js/api/awesome-sms-api.js
@@ -4,8 +4,6 @@ $.when(
     $.getScript("js/api/model/contact.js"),
     $.getScript("js/api/model/message.js"),
     $.getScript("js/api/model/thread.js"),
-    $.getScript("js/api/network/network.js"),
-    $.getScript("js/api/util/data-store.js"),
     $.getScript("js/api/util/util.js")
 ).then(function () {
     console.log("AwesomeSMS api v0.1.0");
